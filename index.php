6793bb12472a8189b5f8faa5e9e5b70293d58c02
Narek Vardzelyan <narek.vardzelyan@of.am>
2015-11-04 19:04:23 +0400

    Added SeleniumOneToSeleniumToConverter class with it's methods and SeleniumTwoToBehatMinkFormatter class, but it's still incomplete.

--- index.php
+++ index.php
@@ -1,2 +1,79 @@
 <?php
 
+class SeleniumOneToSeleniumTwoConverter
+{
+    protected $commandsTableXMLIterator;
+    
+    public function __construct($filename)
+    {
+        $fileContent = file_get_contents($filename);
+        $this->commandsTableXMLIterator = new SimpleXMLElementIterator($fileContent);
+//        var_dump($this->commandsTable);
+    }
+    
+    protected function getCommandsListTable()
+    {
+        while($this->commandsTableXMLIterator->current() !== null) {
+            if ($this->commandsTableXMLIterator->current()->getName() === 'table') {
+                return $this->commandsTableXMLIterator->current();
+            }
+            $this->commandsTableXMLIterator->current()->next();
+        }
+        return null;
+    }
+    
+    protected function getCommandsList()
+    {
+        $commandsListTableXMLIterator = $this->getCommandsListTable();
+        $commandsList = [];
+        while ($commandsListTableXMLIterator->current() !== null) {
+            if ($commandsListTableXMLIterator->current()->getName() === 'tr') {
+                $childrens = $commandsListTableXMLIterator->current()->getChildren();
+                $command = $childrens->__toString();
+                $childrens->next();
+                $target = $childrens->__toString();
+                $childrens->next();
+                $value = $childrens->__toString();
+                $commandsList[] = [
+                    $command,
+                    $target,
+                    $value
+                ];
+            }
+        }
+        return $commandsList;
+    }
+    
+    public function convert()
+    {
+        $commandsList = $this->getCommandsList();
+        $commandsCount = count($CommandsList);
+        $commandsListInSeleniumTwo = [];
+        for ($i = 0; $i < $commandsCount; ++$i) {
+            $commandsListInSeleniumTwo[] = $this->appropriateCommandForSeleniumTwo($commandsList[$i]);
+        }
+    }
+    
+    protected function appropriateCommandForSeleniumTwo($command)
+    {
+        
+    }
+}
+
+class SeleniumTwoToBehatMinkFormatter
+{
+    public function __construct()
+    {
+        
+    }
+    
+    public function format()
+    {
+        
+    }
+}
+
+$fileOne = '/Users/narek_vardzelyan/Downloads/10 hour long Training Session is created.xhtml';
+$fileTwo = '/Users/narek_vardzelyan/Documents/search_wikipedia_html_testcase.html';
+
+$converter = new SeleniumOneToSeleniumTwoConverter($fileOne);
