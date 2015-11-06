1d9939a35dfa684eec9793b4681dfbc1f40667e9
Narek Vardzelyan <narek.vardzelyan@of.am>
2015-11-06 19:02:55 +0400

    Added method toBehatMink, it's for changeing every SeleniumOne command to Behat Mink method, it wroten by switch but in the feuture I'll change it to factory.

--- index.php
+++ index.php
@@ -1,166 +1,48 @@
 <?php
 
-//class SeleniumOneToSeleniumTwoConverter
-//{
-//    protected $fileContent;
-//    protected $commandsTableXMLIterator;
-//    
-//    public function __construct($filename)
-//    {
-//        $this->fileContent = file_get_contents($filename);
-//        $this->commandsTableXMLIterator = new SimpleXMLIterator($this->fileContent);
-//    }
-//    
-//    public function getTestName()
-//    {
-//        $domNode = new DOMDocument('1.0', 'UTF-8');
-//        $domNode->loadXML($this->fileContent);
-//        $title = $domNode->getElementsByTagName('title')[0];
-//        return $title->nodeValue;
-//    }
-//    
-//    protected function getCommandsListTable()
-//    {
-//        while($this->commandsTableXMLIterator->current() !== null) {
-//            if ($this->commandsTableXMLIterator->current()->getName() === 'table') {
-//                return $this->commandsTableXMLIterator->current();
-//            }
-//            $this->commandsTableXMLIterator->current()->next();
-//        }
-//        return null;
-//    }
-//    
-//    protected function getCommandsList()
-//    {
-//        $commandsListTableXMLIterator = $this->getCommandsListTable();
-//        $commandsList = [];
-//        while ($commandsListTableXMLIterator->current() !== null) {
-//            if ($commandsListTableXMLIterator->current()->getName() === 'tr') {
-//                $childrens = $commandsListTableXMLIterator->current()->getChildren();
-//                $command = $childrens->__toString();
-//                $childrens->next();
-//                $target = $childrens->__toString();
-//                $childrens->next();
-//                $value = $childrens->__toString();
-//                $commandsList[] = [
-//                    $command,
-//                    $target,
-//                    $value
-//                ];
-//            }
-//        }
-//        return $commandsList;
-//    }
-//    
-//    public function convert()
-//    {
-//        $commandsList = $this->getCommandsList();
-//        $commandsCount = count($commandsList);
-//        $commandsListInSeleniumTwo = [];
-//        for ($i = 0; $i < $commandsCount; ++$i) {
-//            $commandsListInSeleniumTwo[] = $this->appropriateCommandForSeleniumTwo($commandsList[$i]);
-//        }
-//    }
-//    
-//    protected function appropriateCommandForSeleniumTwo($command)
-//    {
-//        switch($command[0]) {
-//            case 'open' :
-//                return [
-//                    'get',
-//                    $command[1]
-//                ];
-//                break;
-//            case 'type' :
-//                return [
-//                    'sendKeys',
-//                    $command[1],
-//                    $command[2]
-//                ];
-//                break;
-//            case 'clickAndWait':
-//                return [
-//                    'click',
-//                    '//button'
-//                ];
-//                break;
-//            case 'select' :
-//                return [
-//                    "findElement(ById('{$command[1]}'))",
-//                    $command[2]
-//                ];
-//                break;
-//        }
-//    }
-//    
-//    protected function open()
-//    {
-//        
-//    }
-//}
-//
-//class SeleniumTwoToBehatMinkFormatter
-//{
-//    protected $contextFileContent;
-//    protected $featureFileContent;
-//    protected $converter;
-//    
-//    public function __construct($fileName)
-//    {
-//        $this->contextFileContent = <<<FILE
-//<?php
-//
-//use Behat\MinkExtension\Context\MinkContext;
-//
-///**
-// * Features context.
-// */
-//class FeatureContext extends MinkContext
-//{
-//    /**
-//     * Initializes context.
-//     * Every scenario gets it's own context object.
-//     *
-//     * @param array \$parameters context parameters (set them up through behat.yml)
-//     */
-//    public function __construct(array \$parameters)
-//    {
-//        // Initialize your context here
-//    }
-//
-//FILE;
-//        $this->featureFileContent = <<<FILE
-//Feature: Search
-//    In order to see a word definition
-//    As a website user
-//    I need to be able to search for a word
-//
-//    @javascript
-//    Scenario: Searching for a page with autocompletion
-//        Given I am on "/wiki/Main_Page"
-//FILE;
-//        $this->converter = new SeleniumOneToSeleniumTwoConverter($fileName);
-//    }
-//    
-//    public function format()
-//    {
-//        $testName = str_replace(' ', '', ucwords(
-//            $this->converter->getTestName()
-//        ));
-//        $this->contextFileContent .= "\n    public function {$testName}()\n    {";
-//        var_dump($this->contextFileContent);
-//    }
-//}
-
 class SeleniumOneToBehatMinkFormatter
 {
     protected $fileContent;
-    protected $commandsTableXMLIterator;
+    protected $testCaseXMLIterator;
+    protected $contextFileContent;
+    protected $featureFileContent;
     
     public function __construct($filename)
     {
         $this->fileContent = file_get_contents($filename);
-        $this->commandsTableXMLIterator = new SimpleXMLIterator($this->fileContent);
+        $this->testCaseXMLDOM = new DOMDocument($this->fileContent);
+        $this->contextFileContent = <<<FILE
+<?php
+
+use Behat\MinkExtension\Context\MinkContext;
+
+/**
+ * Features context.
+ */
+class FeatureContext extends MinkContext
+{
+    /**
+     * Initializes context.
+     * Every scenario gets it's own context object.
+     *
+     * @param array \$parameters context parameters (set them up through behat.yml)
+     */
+    public function __construct(array \$parameters)
+    {
+        // Initialize your context here
+    }
+
+FILE;
+        $this->featureFileContent = <<<FILE
+Feature: Search
+    In order to see a word definition
+    As a website user
+    I need to be able to search for a word
+
+    @javascript
+    Scenario: Searching for a page with autocompletion
+        Given I am on "/wiki/Main_Page"
+FILE;
     }
     
     public function getTestName()
@@ -173,11 +55,9 @@ class SeleniumOneToBehatMinkFormatter
     
     protected function getCommandsListTable()
     {
-        while($this->commandsTableXMLIterator->current() !== null) {
-            if ($this->commandsTableXMLIterator->current()->getName() === 'table') {
-                return $this->commandsTableXMLIterator->current();
-            }
-            $this->commandsTableXMLIterator->current()->next();
+        $elements = $this->testCaseXMLDOM->getElementsByTagName();
+        if (count($elements) !== 0) {
+            return $elements[0];
         }
         return null;
     }
@@ -220,22 +100,23 @@ class SeleniumOneToBehatMinkFormatter
         try {
             switch($command[0]) {
                 case 'open':
-                    
+                    $minkCommands .= "\t\$this->getSession()->visit({$command[1]});\n";
                     break;
                 case 'type':
-
+                    $minkCommands .= "\t\$this->getSession()->fillFields({$command[1]}, {$command[2]});\n";
                     break;
                 case 'clickAndWait':
-
+                    $minkCommands .= "\t\$this->getSession()->pressButton({$command[1]});\n";
+                    $minkCommands .= "\t\$this->getSession()->wait();\n";
                     break;
                 case 'select':
-
+                    $minkCommands .= "\t\$this->getSession()->selectOption({$command[1]}, {$command[2]});\n";
                     break;
                 case 'sendKeys':
-
+                    $minkCommands .= "\t\$this->getSession()->fillFields({$command[1]}, {$command[2]});\n";
                     break;
                 case 'waitForElementPresent':
-
+                    $minkCommands .= "\t\$this->getSession()->wait(10000, 'document.getElementById('{$command[1]}')')";
                     break;
                 default:
                     throw new Exception('There are not command with name '.$command[0]);
@@ -243,13 +124,25 @@ class SeleniumOneToBehatMinkFormatter
         } catch (Exception $ex) {
             die($e->getMessage());
         }
-            
-        
+        return $minkCommands;
+    }
+    
+    public function format()
+    {
+        $testName = str_replace(' ', '', ucwords(
+            $this->getTestName()
+        ));
+        $this->contextFileContent .= "\n    public function {$testName}()\n    {";
+        $commands = $this->getCommandsList();
+        foreach ($commands as $command) {
+            $this->contextFileContent .= $this->toBehatMink($command);
+        }
+        var_dump($this->contextFileContent);
     }
 }
 
-$fileOne = '/home/narek/Downloads/10 hour long Training Session is created.xhtml';
+$fileOne = '/Users/narek_vardzelyan/Documents/10 hour long Training Session is created.xhtml';
 $fileTwo = '/Users/narek_vardzelyan/Documents/search_wikipedia_html_testcase.html';
 
-$formatter = new SeleniumTwoToBehatMinkFormatter($fileOne);
+$formatter = new SeleniumOneToBehatMinkFormatter($fileOne);
 $formatter->format();
